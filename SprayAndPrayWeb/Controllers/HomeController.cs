57332d279005df5d9bc8cb1a723e63e865d7ab8d
NotHotDogX@gmail.com <NotHotDogX@gmail.com>
2022-06-24 21:35:22 -0400

    Adding DB, first table, entity framework core, etc

--- SprayAndPrayWeb/Controllers/HomeController.cs
+++ SprayAndPrayWeb/Controllers/HomeController.cs
@@ -7,10 +7,14 @@ namespace SprayAndPrayWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
---
+++ SprayAndPrayWeb/Models/Category.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SprayAndPrayWeb.Models
+{
+    public class Category
+    {
+        /// <summary>
+        ///     Id: Primary Key and Identity column
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        ///     Name: Required field
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Display order
+        /// </summary>
+        public int DisplayOrder { get; set; }
+
+        /// <summary>
+        ///     Created DateTime
+        /// </summary>
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
